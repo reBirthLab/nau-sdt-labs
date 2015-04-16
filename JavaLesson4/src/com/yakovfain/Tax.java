a19f2dc079678a8e6bbe194047c918a17b12c025
Anastasiy <Anastasiy@Anastasiy-PC>
2015-04-16 19:01:59 +0300

    Merge origin/master
    Conflicts:
    	JavaLesson3/nbproject/project.properties
    	JavaLesson3/src/com/yakovfain/JavaLesson3.java

--- JavaLesson4/src/com/yakovfain/Tax.java
+++ JavaLesson4/src/com/yakovfain/Tax.java
@@ -10,5 +10,29 @@ package com.yakovfain;
  * @author Anastasiy
  */
 public class Tax {
+    double grossIncome; // class member variables
+    String state;
+    int dependents;
+    static int customerCounter;
     
+    // Constructor
+    Tax (double gi, String st, int depen){
+        grossIncome = gi; // member variable initialization
+        state = st;
+        dependents = depen;
+        customerCounter++; // increment the counter by one
+        
+        System.out.println("Prepering the tax data for customer #" + customerCounter);
+    }
+    
+    // Tax Calculations
+    double calcTax(){
+        return (grossIncome*0.33 - dependents*100);
+    }
+    
+    // USD to EUR Converter
+    static void convertInEuros (double tax){
+        double taxInEuros = tax * 1.25;
+        System.out.println("This tax in Euros: " + taxInEuros);
+    };
 }
--- JavaLesson4/src/com/yakovfain/TestTax.java
+++ JavaLesson4/src/com/yakovfain/TestTax.java
@@ -11,6 +11,30 @@ package com.yakovfain;
  */
 public class TestTax {
     public static void main (String[] args){
+        double grossIncome; // local variables
+        String state;
+        int dependants;
+        
+        grossIncome = 50000;
+        dependants = 2;
+        state = "NJ";
+        
+        Tax t = new Tax(grossIncome, state, dependants);
+        
+        double yourTax = t.calcTax(); // calculating tax
+        
+        Tax t2 = new Tax(65000, "TX", 4);
+        
+        double hisTax = t2.calcTax();
+        
+        // Printing the result
+        System.out.println("Your tax is " + yourTax);
+        System.out.println("Your tax is " + hisTax);
+        
+        // Convert taxes in Euros
+        t.convertInEuros(yourTax);
+        t2.convertInEuros(hisTax);
+        
         
     }
 }
